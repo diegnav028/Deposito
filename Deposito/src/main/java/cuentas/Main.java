b5c669dcd3845d6b8b5fc9c641722cfc4d99b76d
diegnav028 <60139294+diegnav028@users.noreply.github.com>
2023-03-14 17:22:54 +0100

    Add files via upload

---
+++ Deposito/src/main/java/cuentas/Main.java
@@ -0,0 +1,34 @@
+package cuentas;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+          CCuenta cuenta1;
+          cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+   operativa_cuenta(500, cuenta1);
+    }
+    private static void operativa_cuenta(float cantidad , CCuenta cuenta1){
+    
+        double saldoActual;
+
+        
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es "+ saldoActual );
+
+        try {
+            cuenta1.retirar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+        
+        
+    }
+}
